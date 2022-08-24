02a277bd113e6226ef37022a98db044f0510c57b
SuryaJD <SuryaJD@users.noreply.github.com>
2022-08-24 19:59:12 +0530

    Add lead Update Workflow

--- app/Http/Controllers/Invoker/WebhookHandler.php
+++ app/Http/Controllers/Invoker/WebhookHandler.php
@@ -29,16 +29,76 @@ class WebhookHandler extends Controller
             )
         ]);
 
+
         Log::info($request->all());
 
         $subject = 'You got a new SNS Message';
 
-        $message = json_encode($request->all());
+        $message = json_encode([
+            "entity_name" => $request->lastname,
+            "entity_email" => $request->cf_872,
+            "entity_phone" => $request->cf_896,
+            "entity_address_l1" => $request->cf_862,
+            "entity_address_l2" => $request->cf_864,
+            "entity_pin" => $request->cf_900 ,
+            "entity_dist" => $request->cf_868,
+            "entity_state" => $request->state,
+            "entity_country" => 'India',
+            "site_address_l1" => $request->cf_876,
+            "site_address_l2" => $request->cf_878,
+            "site_pin"  => $request->cf_898,
+            "site_dist" => $request->cf_882,
+            "site_state" => $request->cf_884,
+            "site_country" => 'India',
+            "installer_lead_id" => $request->cf_894,
+            "status" => $request->cf_886,
+            "remark" => $request->cf_888,
+            "proposed_project_size_kw" => $request->cf_874,
+            "electric_bill" => $request->cf_906
+        ]);
+
+        // Below data In Id +
+        // "entity_name": "hello1",
+        // "entity_email": "entityEmail@email.com",
+        // "entity_phone": "7831940099",
+        // "entity_address_l1": "entityAddressL11",
+        // "entity_address_l2": "entity addres L2",
+        // "entity_pin": "123456",
+        // "entity_dist": "entityDist1",
+        // "entity_state": "entityState1",
+        // "entity_country": "entityCountry1",
+        // "electric_bill": "electricbill",
+        // "proposal": "proposal",
+        // "site_address_l1": "siteAddressL11",
+        // "site_address_l2": "siteAddressL21",
+        // "site_pin": "654321",
+        // "site_dist": "siteDist1",
+        // "site_state": "siteState1",
+        // "site_country": "siteCountry1",
+        // "proposed_project_size_kw": "`123",
+        // "proposed_project_area_sqmtr": "321"
 
         $client->publish([
             'TopicArn' => 'arn:aws:sns:ap-south-1:838923037017:model_sync',
             'Message' => $message,
-            'Subject' => $subject
+            'Subject' => $subject,
+            'MessageAttributes' => [
+                'Application' => [
+                    'DataType'    => 'String',
+                    'StringValue' => 'vtiger'
+                ],
+                'Module'       => [
+                    'DataType'    => 'String',
+                    'StringValue' => 'Leads'
+                ],
+                'Trigger'     => [
+                    'DataType'    => 'String',
+                    'StringValue' => 'Leads.updated'
+                ]
+                // 'Module'      => 'Leads',
+                // 'Trigger'     => 'lead.updated',
+                // 'Task'        => 'Leads Status Updated',
+            ]
         ]);
     }
 }
--- app/Http/Controllers/VtigerProxyApi/LeadController.php
+++ app/Http/Controllers/VtigerProxyApi/LeadController.php
@@ -29,35 +29,65 @@ class LeadController extends Controller
     {
         $notification = json_decode($request->getContent(), true);
 
-        Log::info('Data',$notification);
+        Log::info('notification', $notification);
 
-        $data = json_decode($notification['Message'],true);
+        $data = json_decode($notification['Message'], true);
 
-        $response = Http::post("https://crm.aerem.co/modules/Webforms/capture.php", [
-            '__vtrftk'          => 'sid:5fd6c32e68d77ecd612ee689833d51c72e9c19ce,1661181177',
-            'publicid'          => '883bcfb8fb3729fed71784919bb7c685',
-            'urlencodeenable'   => '1',
-            'name'              => 'Lead Creation Api',
-            'lastname'          =>  $data['entity_name'],
-            'cf_862'            => 'Address Line 1',
-            'cf_864'            => 'Address Line 2',
-            'cf_866'            => 'Pincode',
-            'cf_868'            => 'District',
-            'state'             => 'state',
-            'cf_870'            => 'Mobile number',
-            'cf_872'            =>  $data['entity_email'],
-            'cf_874'            => 'Project Size',
-            'cf_876'            => 'Installation Address Line One',
-            'cf_878'            => 'Installation Address Line Two',
-            'cf_880'            => 'Installation Pincode',
-            'cf_882'            => 'Installation District',
-            'cf_884'            => 'Installation State',
-        ]);
+        // Log::error($notification['MessageAttributes']['Application']['Value']);
 
-        Log::info($response->body());
+        if (isset($notification['MessageAttributes']) && $notification['MessageAttributes']['Application']['Value'] == 'aerem') {
+            Log::info("message", $data);
 
+            $response = Http::asForm()->withOptions([
+                'version' => 2,
+            ])->post("https://crm.aerem.co/modules/Webforms/capture.php", [
+                '__vtrftk'          => 'sid:5fd6c32e68d77ecd612ee689833d51c72e9c19ce,1661181177',
+                'publicid'          => '883bcfb8fb3729fed71784919bb7c685',
+                'urlencodeenable'   => '1',
+                'name'              => 'Lead Creation Api',
+                'lastname'          =>  $data['entity_name'], // Name
+                'cf_862'            =>  $data['entity_address_l1'],  // 'Address Line 1',
+                'cf_864'            =>  $data['entity_address_l2'],  // 'Address Line 2',
+                'cf_900'            =>  $data['entity_pin'], //'Pincode',
+                'cf_868'            =>  $data['entity_dist'], // 'District',
+                'state'             =>  $data['entity_state'], //'state',
+                'cf_896'            =>  $data['entity_phone'], //'Mobile number',
+                'cf_872'            =>  $data['entity_email'], // Email
+                'cf_874'            =>  $data['proposed_project_size_kw'], // 'Project Size',
+                'cf_876'            =>  $data['site_address_l1'], // 'Installation Address Line One',
+                'cf_878'            =>  $data['site_address_l2'], //'Installation Address Line Two',
+                'cf_898'            =>  $data['site_pin'], //'Installation Pincode',
+                'cf_882'            =>  $data['site_dist'], // 'Installation District',
+                'cf_884'            =>  $data['site_state'], // 'Installation State',
+                'cf_894'            =>  strval($data['installer_lead_id']),
+                'cf_906'            =>  $data['electric_bill']
+            ]);
 
+            Log::debug('Sent to vtiger', [
+                '__vtrftk'          => 'sid:5fd6c32e68d77ecd612ee689833d51c72e9c19ce,1661181177',
+                'publicid'          => '883bcfb8fb3729fed71784919bb7c685',
+                'urlencodeenable'   => '1',
+                'name'              => 'Lead Creation Api',
+                'lastname'          =>  $data['entity_name'], // Name
+                'cf_862'            =>  $data['entity_address_l1'],  // 'Address Line 1',
+                'cf_864'            =>  $data['entity_address_l2'],  // 'Address Line 2',
+                'cf_900'            =>  $data['entity_pin'], //'Pincode',
+                'cf_868'            =>  $data['entity_dist'], // 'District',
+                'state'             =>  $data['entity_state'], //'state',
+                'cf_896'            =>  $data['entity_phone'], //'Mobile number',
+                'cf_872'            =>  $data['entity_email'], // Email
+                'cf_874'            =>  $data['proposed_project_size_kw'], // 'Project Size',
+                'cf_876'            =>  $data['site_address_l1'], // 'Installation Address Line One',
+                'cf_878'            =>  $data['site_address_l2'], //'Installation Address Line Two',
+                'cf_898'            =>  $data['site_pin'], //'Installation Pincode',
+                'cf_882'            =>  $data['site_dist'], // 'Installation District',
+                'cf_884'            =>  $data['site_state'], // 'Installation State',
+                'cf_894'            =>  strval($data['installer_lead_id']),
+                'cf_906'            =>  $data['electric_bill']
+            ]);
 
+            Log::info($response->body());
+        }
     }
 
     /**
