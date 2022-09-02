0fb222d3bd72ba85c31b48333c63553bd9ccd65a
SuryaJD <SuryaJD@users.noreply.github.com>
2022-09-02 13:30:47 +0530

    Add intention

--- app/Http/Controllers/Invoker/SnsReceiver.php
+++ app/Http/Controllers/Invoker/SnsReceiver.php
@@ -17,6 +17,23 @@ class SnsReceiver extends Controller
      */
     public function __invoke(Request $request)
     {
-        (new LeadController)->store($request);
+        $this->getIntention($request);
+    }
+
+    public function getIntention($request)
+    {
+        $notification = json_decode($request->getContent(), true);
+        if (isset($notification['MessageAttributes']) && $notification['MessageAttributes']['Application']['Value'] == 'aerem') {
+            $data = json_decode($notification['Message'], true);
+
+            if(isset($data['isResubmited']) && $data['isResubmited']){
+                (new LeadController)->update($request);
+
+            }else {
+                (new LeadController)->store($request);
+            }
+        }else{
+                abort(403);
+        }
     }
 }
--- app/Http/Controllers/VtigerProxyApi/LeadController.php
+++ app/Http/Controllers/VtigerProxyApi/LeadController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\VtigerProxyApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
+use App\Models\LeadCustomField;
 use Illuminate\Support\Facades\Http;
+use Salaros\Vtiger\VTWSCLib\WSClient;
 
 class LeadController extends Controller
 {
@@ -38,10 +40,6 @@ class LeadController extends Controller
             $data = json_decode($notification['Message'], true);
 
             $formData =  [
-                        '__vtrftk'          => 'sid:5fd6c32e68d77ecd612ee689833d51c72e9c19ce,1661181177',
-                        'publicid'          => '883bcfb8fb3729fed71784919bb7c685',
-                        'urlencodeenable'   => '1',
-                        'name'              => 'Lead Creation Api',
                         'lastname'          =>  $data['entity_name'], // Name
                         'cf_862'            =>  $data['entity_address_l1'],  // 'Address Line 1',
                         'cf_864'            =>  $data['entity_address_l2'],  // 'Address Line 2',
@@ -59,42 +57,43 @@ class LeadController extends Controller
                         'cf_894'            =>  strval($data['installer_lead_id']),
                         'cf_908'            =>  $data['electric_bill'],
                         'cf_910'            =>  $data['proposal'],
-                        // 'cf_912'            =>  $data['installer_id'],
-                        // 'cf_918'            =>  $data['installer_name'],
-                        // 'cf_914'            =>  $data['installer_email'],
-                        // 'cf_916'            =>  $data['installer_contact'],
+                        'cf_886'            =>  'PENDING'
             ];
 
             if (isset($data['installer']) && is_array($data['installer'])) {
-                $formData['cf_912']            =  $data['installer']['id'];
-                $formData['cf_918']            =  $data['installer']['name'];
-                $formData['cf_914']            =  $data['installer']['email'];
-                $formData['cf_916']            =  $data['installer']['contact'];
+                $formData['cf_912']            =  $data['installer']['id'] ?? null;
+                $formData['cf_918']            =  $data['installer']['name'] ?? null;
+                $formData['cf_914']            =  $data['installer']['email'] ?? null;
+                $formData['cf_916']            =  $data['installer']['contact'] ?? null;
             }
 
             if (isset($data['channel_manager']) && is_array($data['channel_manager'])) {
-                $formData['cf_930']            =  $data['channel_manager']['id'];
-                $formData['cf_932']            =  $data['channel_manager']['name'];
-                $formData['cf_938']            =  $data['channel_manager']['email'];
-                $formData['cf_936']            =  $data['channel_manager']['contact'];
+                $formData['cf_930']            =  $data['channel_manager']['id'] ?? null;
+                $formData['cf_932']            =  $data['channel_manager']['name'] ?? null;
+                $formData['cf_938']            =  $data['channel_manager']['email'] ?? null;
+                $formData['cf_936']            =  $data['channel_manager']['contact'] ?? null;
             }
 
             if (isset($data['loan_sales_manager']) && is_array($data['loan_sales_manager'])) {
-                $formData['cf_942']            =  $data['loan_sales_manager']['id'];
-                $formData['cf_944']            =  $data['loan_sales_manager']['name'];
-                $formData['cf_948']            =  $data['loan_sales_manager']['email'];
-                $formData['cf_946']            =  $data['loan_sales_manager']['contact'];
+                $formData['cf_942']            =  $data['loan_sales_manager']['id'] ?? null;
+                $formData['cf_944']            =  $data['loan_sales_manager']['name'] ?? null;
+                $formData['cf_948']            =  $data['loan_sales_manager']['email'] ?? null;
+                $formData['cf_946']            =  $data['loan_sales_manager']['contact'] ?? null;
             }
 
             Log::info("message", $data);
 
-            $response = Http::asForm()->withOptions([
-                'version' => 2,
-            ])->post("https://crm.aerem.co/modules/Webforms/capture.php", $formData);
+            $client = new WSClient('https://crm.aerem.co/', 'admin', 'Pt6Oh5A3YhofNY3');
 
-            Log::debug('Sent to vtiger', $formData);
+            try {
+                $added = $client->entities->createOne('Leads', $formData);
+                Log::info('Lead Added',$added);
+            } catch (\Throwable $th) {
+                Log::error('vtiger webservice error',[
+                    'error' => $th->getMessage(),
+                ]);
+            }
 
-            Log::info($response->body());
         }
     }
     /**
@@ -115,9 +114,72 @@ class LeadController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        $notification = json_decode($request->getContent(), true);
+
+        if (isset($notification['MessageAttributes']) && $notification['MessageAttributes']['Application']['Value'] == 'aerem') {
+
+            $data = json_decode($notification['Message'], true);
+
+            $leadId = optional(LeadCustomField::where(['cf_894' => $data['installer_lead_id']])->first())->leadId;
+
+            $formData =  [
+                        'lastname'          =>  $data['entity_name'], // Name
+                        'cf_862'            =>  $data['entity_address_l1'],  // 'Address Line 1',
+                        'cf_864'            =>  $data['entity_address_l2'],  // 'Address Line 2',
+                        'cf_900'            =>  $data['entity_pin'], //'Pincode',
+                        'cf_868'            =>  $data['entity_dist'], // 'District',
+                        'state'             =>  $data['entity_state'], //'state',
+                        'cf_896'            =>  $data['entity_phone'], //'Mobile number',
+                        'cf_872'            =>  $data['entity_email'], // Email
+                        'cf_874'            =>  $data['proposed_project_size_kw'], // 'Project Size',
+                        'cf_876'            =>  $data['site_address_l1'], // 'Installation Address Line One',
+                        'cf_878'            =>  $data['site_address_l2'], //'Installation Address Line Two',
+                        'cf_898'            =>  $data['site_pin'], //'Installation Pincode',
+                        'cf_882'            =>  $data['site_dist'], // 'Installation District',
+                        'cf_884'            =>  $data['site_state'], // 'Installation State',
+                        'cf_894'            =>  strval($data['installer_lead_id']),
+                        'cf_908'            =>  $data['electric_bill'],
+                        'cf_910'            =>  $data['proposal'],
+                        'cf_886'            =>  $data['status']
+            ];
+
+            if (isset($data['installer']) && is_array($data['installer'])) {
+                $formData['cf_912']            =  $data['installer']['id'] ?? null;
+                $formData['cf_918']            =  $data['installer']['name'] ?? null;
+                $formData['cf_914']            =  $data['installer']['email'] ?? null;
+                $formData['cf_916']            =  $data['installer']['contact'] ?? null;
+            }
+
+            if (isset($data['channel_manager']) && is_array($data['channel_manager'])) {
+                $formData['cf_930']            =  $data['channel_manager']['id'] ?? null;
+                $formData['cf_932']            =  $data['channel_manager']['name'] ?? null;
+                $formData['cf_938']            =  $data['channel_manager']['email'] ?? null;
+                $formData['cf_936']            =  $data['channel_manager']['contact'] ?? null;
+            }
+
+            if (isset($data['loan_sales_manager']) && is_array($data['loan_sales_manager'])) {
+                $formData['cf_942']            =  $data['loan_sales_manager']['id'] ?? null;
+                $formData['cf_944']            =  $data['loan_sales_manager']['name'] ?? null;
+                $formData['cf_948']            =  $data['loan_sales_manager']['email'] ?? null;
+                $formData['cf_946']            =  $data['loan_sales_manager']['contact'] ?? null;
+            }
+
+            Log::info("message", $data);
+
+            $client = new WSClient('https://crm.aerem.co/', 'admin', 'Pt6Oh5A3YhofNY3');
+
+            try {
+                $added = $client->entities->updateOne('Leads', $leadId ,$formData);
+                Log::info('Lead updated',$added);
+            } catch (\Throwable $th) {
+                Log::error('vtiger webservice error',[
+                    'error' => $th->getMessage(),
+                ]);
+            }
+
+        }
     }
 
     /**
