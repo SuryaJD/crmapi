4175e8673923c01230cb2781ef9885adb39d721b
SuryaJD <SuryaJD@users.noreply.github.com>
2022-09-20 23:15:31 +0530

    Added host support for local

--- config/vtiger.php
+++ config/vtiger.php
@@ -3,6 +3,7 @@
 
 return [
     'vtiger_host' => [
+        'local' => env('VTIGER_HOST_DEV'),
         'dev' => env('VTIGER_HOST_DEV'),
         'production' => env('VTIGER_HOST_PRODUCTION')
     ],
